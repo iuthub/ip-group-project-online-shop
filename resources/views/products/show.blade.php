b8460ccd95870c14ac7c750993ca6dc9bb76bcf1
TheSTIG1997 <39194312+TheSTIG1997@users.noreply.github.com>
2020-05-03 12:08:29 +0500

    Item update 2

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -4,11 +4,11 @@
     <div class="row justify-content-center py-4">
         <h1 class="display-3">Item</h1>
     </div>
-    <div class="row align-items-center rounded shadow">
-        <div class="col-md-6 bg-danger align-self-center image-position">
+    <div class="row align-items-center">
+        <div class="col-md-6 bg-light align-self-center image-position">
             <img style="width:100%" src="https://www.kindpng.com/picc/m/134-1345320_chocolate-png-image-chocolate-png-transparent-png.png">
         </div>
-        <div class="col-md-6 bg-warning px-5 forms">
+        <div class="col-md-6 bg-light px-5 forms">
             <div class="form-group lead">
                 <strong class="lead d-flex">Brand:</strong>
                 {{ $product->brand }}
@@ -29,6 +29,7 @@
                 <strong class="lead d-flex">Description:</strong>
                 {{ $product->description }}
             </div>
+            <br>
             <h2 class="lead d-flex justify-content-center text-primary text-uppercase">Brief description</h2>
             <p style="text-indent: 20px;" class="lead px-2 text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis minima facere impedit quibusdam officia necessitatibus nihil nulla quas quod magni tempore aperiam, reiciendis, accusantium laudantium asperiores, nam consectetur obcaecati dolore alias omnis ad ex cupiditate qui? Aspernatur excepturi possimus nesciunt ad voluptate harum, sint alias maxime totam numquam molestias! Minima accusantium nostrum ipsam quo quibusdam, aut, vero nulla dolor repellat saepe similique et beatae molestias libero laboriosam neque earum at. Maxime eligendi aliquam veniam debitis non ex voluptatum molestiae. Veritatis quae eveniet, illum neque inventore vitae ut fugiat facilis sit at debitis similique deserunt tempora molestiae reprehenderit est aliquid qui.</p>
         </div>
