6ca5508234618d017a133d6ddd292b715745bba9
aprilau <k.aripova@student.inha.uz>
2020-05-06 21:56:58 +0500

    show products from database on the main page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Product;
+
 
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('pages.index');
+        $products = Product::all();
+        return view('pages.index',compact('products'));
     }
     
     public function adminItem() {
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,25 +12,12 @@
       <img src="https://images.unsplash.com/photo-1511381939415-e44015466834?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1438&q=80" class="d-block w-100" alt="Dark Chocolate Image">
       <div class="carousel-caption d-none d-md-block">
         <h5>Dark Chocolate</h5>
-      
       </div>
     </div>
     <div class="carousel-item">
       <img src="https://images.pexels.com/photos/867464/pexels-photo-867464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="d-block w-100" alt="Milk Chocolate Image">
       <div class="carousel-caption d-none d-md-block">
         <h5>Milk Chocolate</h5>
-         
-      </div>
-    </div>
-    <div class="filter">
-      
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="https://images.pexels.com/photos/867464/pexels-photo-867464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="d-block w-100" alt="Milk Chocolate Image">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Milk Chocolate</h5>
-         
       </div>
     </div>
     <div class="carousel-item">
@@ -40,107 +27,21 @@
       </div>
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
 <div class="container">
   <div class="row my-4">
+    @foreach ($products as $product)
     <div class="card-wrapper col-md-4 d-flex justify-content-center align-items-center px-1 py-1">
       <div class="card product-card d-flex align-items-center" style="width: 24rem;">
-        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/1776/0569/products/kit_kat_cone_multi-pack_99b2ea46-ca30-4be3-b6cf-16365eb6c1c4_grande.png?v=1513811831" alt="Card image cap">
+        <img class="card-img-top" src="/storage/images/{{$product->image}}" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">$10</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <h5 class="card-title">{{ $product->brand }}</h5>
+          <p class="card-text">Price: ${{ $product->price }}</p>
+          <a href="#" class="btn btn-primary">More...</a>
         </div>
       </div>
     </div>
-    <div class="card-wrapper col-md-4 d-flex justify-content-center align-items-center px-1 py-1">
-      <div class="card d-flex align-items-center" style="width: 24rem;">
-        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/1776/0569/products/kit_kat_cone_multi-pack_99b2ea46-ca30-4be3-b6cf-16365eb6c1c4_grande.png?v=1513811831" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">$10</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="card-wrapper col-md-4 d-flex justify-content-center align-items-center px-1 py-1">
-      <div class="card d-flex align-items-center" style="width: 24rem;">
-        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/1776/0569/products/kit_kat_cone_multi-pack_99b2ea46-ca30-4be3-b6cf-16365eb6c1c4_grande.png?v=1513811831" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">$10</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="card-wrapper col-md-4 d-flex justify-content-center align-items-center px-1 py-1">
-      <div class="card d-flex align-items-center" style="width: 24rem;">
-        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/1776/0569/products/kit_kat_cone_multi-pack_99b2ea46-ca30-4be3-b6cf-16365eb6c1c4_grande.png?v=1513811831" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">$10</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="card-wrapper col-md-4 d-flex justify-content-center align-items-center px-1 py-1">
-      <div class="card d-flex align-items-center" style="width: 24rem;">
-        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/1776/0569/products/kit_kat_cone_multi-pack_99b2ea46-ca30-4be3-b6cf-16365eb6c1c4_grande.png?v=1513811831" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">$10</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="card-wrapper col-md-4 d-flex justify-content-center align-items-center px-1 py-1">
-      <div class="card d-flex align-items-center" style="width: 24rem;">
-        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/1776/0569/products/kit_kat_cone_multi-pack_99b2ea46-ca30-4be3-b6cf-16365eb6c1c4_grande.png?v=1513811831" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">$10</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="card-wrapper col-md-4 d-flex justify-content-center align-items-center px-1 py-1">
-      <div class="card d-flex align-items-center" style="width: 24rem;">
-        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/1776/0569/products/kit_kat_cone_multi-pack_99b2ea46-ca30-4be3-b6cf-16365eb6c1c4_grande.png?v=1513811831" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">$10</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="card-wrapper col-md-4 d-flex justify-content-center align-items-center px-1 py-1">
-      <div class="card d-flex align-items-center" style="width: 24rem;">
-        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/1776/0569/products/kit_kat_cone_multi-pack_99b2ea46-ca30-4be3-b6cf-16365eb6c1c4_grande.png?v=1513811831" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">$10</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="card-wrapper col-md-4 d-flex justify-content-center align-items-center px-1 py-1">
-      <div class="card d-flex align-items-center" style="width: 24rem;">
-        <img class="card-img-top" src="https://cdn.shopify.com/s/files/1/1776/0569/products/kit_kat_cone_multi-pack_99b2ea46-ca30-4be3-b6cf-16365eb6c1c4_grande.png?v=1513811831" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">$10</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
+    @endforeach
   </div>
   <div class="row justify-content-center my-4">
     <ul class="pagination d-flex justify-content-center">
@@ -153,5 +54,4 @@
     </ul>
   </div>
 </div>
-
 @endsection
